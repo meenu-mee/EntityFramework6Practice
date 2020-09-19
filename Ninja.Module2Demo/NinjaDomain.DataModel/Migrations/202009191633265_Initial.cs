071b28f67c82ef3ff51bc08c7696933ac10514f2
Meenu <meenu.mee@outlook.com>
2020-09-19 23:11:07 +0530

    Added migrations

---
+++ Ninja.Module2Demo/NinjaDomain.DataModel/Migrations/202009191633265_Initial.cs
@@ -0,0 +1,59 @@
+﻿namespace NinjaDomain.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clans", // create a table for Clans with below column names and primary key
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ClanName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Ninjas",// create a table for Ninjas with below column names, primary key and forreign key(and its index)
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        ServedInOniwaban = c.Boolean(nullable: false),
+                        ClanId = c.Int(nullable: false),
+                        DateOfBirth = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clans", t => t.ClanId, cascadeDelete: true)
+                .Index(t => t.ClanId);
+            
+            CreateTable(
+                "dbo.NinjaEquipments",// create a table for NinjaEquipments with below column names, primary key and forreign key(and its index)
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Type = c.Int(nullable: false),
+                        Ninja_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Ninjas", t => t.Ninja_Id, cascadeDelete: true)
+                .Index(t => t.Ninja_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.NinjaEquipments", "Ninja_Id", "dbo.Ninjas");
+            DropForeignKey("dbo.Ninjas", "ClanId", "dbo.Clans");
+            DropIndex("dbo.NinjaEquipments", new[] { "Ninja_Id" });
+            DropIndex("dbo.Ninjas", new[] { "ClanId" });
+            DropTable("dbo.NinjaEquipments");
+            DropTable("dbo.Ninjas");
+            DropTable("dbo.Clans");
+        }
+    }
+}
